2e6d04d13420de660da7b10c562877f25ee771f7
a23135362 <a_vaibhav.arya@africa.airtel.com>
2022-11-08 19:00:05 +0530

    CORS

--- src/main/java/com/vaibhav/ecommerce/EcommerceApplication.java
+++ src/main/java/com/vaibhav/ecommerce/EcommerceApplication.java
@@ -9,5 +9,13 @@ public class EcommerceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceApplication.class, args);
 	}
-
+	@Bean
+	public WebMvcConfigurer corsConfigurer(){
+		return new WebMvcConfigurer(){
+			@Override
+			public void addCorsMapping(CorsRegistry registry){
+				registry.addMapping("/").allowedOrigins("https://tranquil-sea-78476.herokuapp.com/");
+			}
+		}
+	}
 }
