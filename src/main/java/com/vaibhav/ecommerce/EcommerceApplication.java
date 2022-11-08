54407caccc3e48afaf92b1d25c679991188472c3
a23135362 <a_vaibhav.arya@africa.airtel.com>
2022-11-08 20:20:41 +0530

    CORS

--- src/main/java/com/vaibhav/ecommerce/EcommerceApplication.java
+++ src/main/java/com/vaibhav/ecommerce/EcommerceApplication.java
@@ -2,9 +2,6 @@ package com.vaibhav.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class EcommerceApplication {
@@ -12,13 +9,5 @@ public class EcommerceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceApplication.class, args);
 	}
-	@Bean
-	public WebMvcConfigurer corsConfigurer(){
-		return new WebMvcConfigurer(){
-			@Override
-			public void addCorsMappings(CorsRegistry registry){
-				registry.addMapping("/").allowedOrigins("https://tranquil-sea-78476.herokuapp.com/");
-			}
-		};
-	}
+
 }
